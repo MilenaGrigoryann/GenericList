ff465ad48f31141de6b4033e38977ef49ee2dc0e
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-06-11 20:44:20 +0400

    Add files via upload

---
+++ GenericList.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public class GenericList<T>
+    {
+        private const int _defaultCapacity = 4;
+        private T[] _items;
+        private int _size;
+        private int _version;
+        static readonly T[] _emptyArray = new T[0];
+        public GenericList()
+        {
+            _items = _emptyArray;
+        }
+        public GenericList(int capacity)
+        {
+            if (capacity < 0) throw new ArgumentOutOfRangeException();
+            if (capacity == 0)
+                _items = _emptyArray;
+            else
+                _items = new T[capacity];
+        }
+        public int Capacity
+        {
+            get
+            {
+                return _items.Length;
+            }
+            set
+            {
+                if (value < _size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                if (value != _items.Length)
+                {
+                    if (value > 0)
+                    {
+                        T[] newItems = new T[value];
+                        if (_size > 0)
+                        {
+                            Array.Copy(_items, 0, newItems, 0, _size);
+                        }
+                        _items = newItems;
+                    }
+                    else
+                    {
+                        _items = _emptyArray;
+                    }
+                }
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
+        public T this[int index]
+        {
+            get
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return _items[index];
+            }
+            set
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _items[index] = value;
+                _version++;
+            }
+        }
+        public void Add(T item)
+        {
+            if (_size == _items.Length) EnsureCapacity(_size + 1);
+            _items[_size++] = item;
+            _version++;
+        }
+        private void EnsureCapacity(int min)
+        {
+            if (_items.Length < min)
+            {
+                int newCapacity = _items.Length == 0 ? _defaultCapacity : _items.Length * 2;
+                if ((uint)newCapacity > 0X7FEFFFFF) newCapacity = 0X7FEFFFFF;
+                if (newCapacity < min) newCapacity = min;
+                Capacity = newCapacity;
+            }
+        }
+        public void AddRange(IEnumerable<T> collection)
+        {
+            InsertRange(_size, collection);
+        }
+        public void InsertRange(int index, IEnumerable<T> collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if ((uint)index > (uint)_size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            ICollection<T> c = collection as ICollection<T>;
+            if (c != null)
+            {
+                int count = c.Count;
+                if (count > 0)
+                {
+                    EnsureCapacity(_size + count);
+                    if (index < _size)
+                    {
+                        Array.Copy(_items, index, _items, index + count, _size - index);
+                    }
+                    if (this == c)
+                    {
+                        Array.Copy(_items, 0, _items, index, index);
+                        Array.Copy(_items, index + count, _items, index * 2, _size - index);
+                    }
+                    else
+                    {
+                        T[] itemsToInsert = new T[count];
+                        c.CopyTo(itemsToInsert, 0);
+                        itemsToInsert.CopyTo(_items, index);
+                    }
+                    _size += count;
+                }
+            }
+            else
+            {
+                using (IEnumerator<T> en = collection.GetEnumerator())
+                {
+                    while (en.MoveNext())
+                    {
+                        Insert(index++, en.Current);
+                    }
+                }
+            }
+            _version++;
+        }
+        public void Insert(int index, T item)
+        {
+            if ((uint)index > (uint)_size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (_size == _items.Length) EnsureCapacity(_size + 1);
+            if (index < _size)
+            {
+                Array.Copy(_items, index, _items, index + 1, _size - index);
+            }
+            _items[index] = item;
+            _size++;
+            _version++;
+        }
+        public void Clear()
+        {
+            _items = _emptyArray;
+            _version++;
+        }
+        public bool Contains(T item)
+        {
+            if ((Object)item == null)
+            {
+                for (int i = 0; i < _size; i++)
+                    if ((Object)_items[i] == null)
+                        return true;
+                return false;
+            }
+            else
+            {
+                EqualityComparer<T> c = EqualityComparer<T>.Default;
+                for (int i = 0; i < _size; i++)
+                {
+                    if (c.Equals(_items[i], item)) return true;
+                }
+                return false;
+            }
+        }
+        public void CopyTo(T[] array)
+        {
+            CopyTo(array, 0);
+        }
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Array.Copy(_items, 0, array, arrayIndex, _size);
+        }
+        public void CopyTo(int index, T[] array, int arrayIndex, int count)
+        {
+            if (_size - index < count)
+            {
+                throw new ArgumentException();
+            }
+            Array.Copy(_items, index, array, arrayIndex, count);
+        }
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+        public struct Enumerator
+        {
+            private GenericList<T> list;
+            private int index;
+            private int version;
+            private T current;
+            internal Enumerator(GenericList<T> list)
+            {
+                this.list = list;
+                index = 0;
+                version = list._version;
+                current = default(T);
+            }
+            public bool MoveNext()
+            {
+                GenericList<T> localList = list;
+                if (version == localList._version && ((uint)index < (uint)localList._size))
+                {
+                    current = localList._items[index];
+                    index++;
+                    return true;
+                }
+                return MoveNextRare();
+            }
+            private bool MoveNextRare()
+            {
+                if (version != list._version)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                index = list._size + 1;
+                current = default(T);
+                return false;
+            }
+            public T Current
+            {
+                get
+                {
+                    return current;
+                }
+            }
+
+        }
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(_items, item, 0, _size);
+        }
+        public int IndexOf(T item, int index)
+        {
+            if (index > _size)
+                throw new ArgumentOutOfRangeException();
+            return Array.IndexOf(_items, item, index, _size - index);
+        }
+        public int IndexOf(T item, int index, int count)
+        {
+            if (index > _size)
+                throw new ArgumentOutOfRangeException();
+            if (count < 0 || index > _size - count) throw new ArgumentOutOfRangeException();
+            return Array.IndexOf(_items, item, index, count);
+        }
+        public int LastIndexOf(T item)
+        {
+            if (_size == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return LastIndexOf(item, _size - 1, _size);
+            }
+        }
+        public int LastIndexOf(T item, int index, int count)
+        {
+            if ((Count != 0) && (index < 0))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if ((Count != 0) && (count < 0))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (_size == 0)
+            {
+                return -1;
+            }
+            if (index >= _size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (count > index + 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return Array.LastIndexOf(_items, item, index, count);
+        }
+        public int LastIndexOf(T item, int index)
+        {
+            if (index >= _size)
+                throw new ArgumentOutOfRangeException();
+            return LastIndexOf(item, index, index + 1);
+        }
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+
+            return false;
+        }
+        public void RemoveAt(int index)
+        {
+            if ((uint)index >= (uint)_size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _size--;
+            if (index < _size)
+            {
+                Array.Copy(_items, index + 1, _items, index, _size - index);
+            }
+            _items[_size] = default(T);
+            _version++;
+        }
+        public void RemoveRange(int index, int count)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (_size - index < count)
+                throw new ArgumentOutOfRangeException();
+            if (count > 0)
+            {
+                int i = _size;
+                _size -= count;
+                if (index < _size)
+                {
+                    Array.Copy(_items, index + count, _items, index, _size - index);
+                }
+                Array.Clear(_items, _size, count);
+                _version++;
+            }
+        }
+        public void Reverse()
+        {
+            Reverse(0, Count);
+        }
+        public void Reverse(int index, int count)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (_size - index < count)
+                throw new ArgumentOutOfRangeException();
+            Array.Reverse(_items, index, count);
+            _version++;
+        }
+        public T[] ToArray()
+        {        
+            T[] array = new T[_size];
+            Array.Copy(_items, 0, array, 0, _size);
+            return array;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<string> list1 = new GenericList<string>();
+            List<string> list2 = new List<string> { "bb", "cc", "dd" };
+            string[] arr = new string[] { "111", "22", "3333","44" ,"5","66"};
+            list1.Add("aaa");
+            list1.Add("h");
+            list1.Add("fdjfh");          
+            list1.AddRange(list2);
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(list1.Contains("bbb"));
+            list1.CopyTo(arr);
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            list1.Insert(2, "mln");
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+            list1.InsertRange(3, list2);
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(list1.LastIndexOf("111"));
+            list1.Remove("bb");
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+            list1.RemoveAt(1);
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+            list1.RemoveRange(3, 2);
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+            list1.Reverse();
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+            for (int i = 0; i < list1.ToArray().Length; i++)
+            {
+                Console.WriteLine(list1.ToArray()[i]);
+            }
+        }
+    }
+}
